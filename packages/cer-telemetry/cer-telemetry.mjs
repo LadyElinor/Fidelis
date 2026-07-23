bbf2bced322e14aae89ac9e853c88e29fcfa267b
LadyElinor <LadyElinor@users.noreply.github.com>
2026-07-23 14:19:29 -0400

    normalize cer telemetry package surface

---
+++ packages/cer-telemetry/cer-telemetry.mjs
@@ -0,0 +1,3 @@
+export { validateReceipt, writeReceipt } from './tools/cer-telemetry/receipt.js';
+export { checkInvariants } from './tools/cer-telemetry/invariants.js';
+export { rateDelta, wilsonInterval } from './tools/cer-telemetry/stats.js';
